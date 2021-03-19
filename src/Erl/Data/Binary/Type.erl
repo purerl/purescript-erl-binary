d6665ea3c186e5364ee353cd3ceed7aed1b28a94
robashton <robashton@codeofrob.com>
2021-03-19 14:16:05 +0000

    add monoid/semigroup to erlang binary type

--- src/Erl/Data/Binary/Type.erl
+++ src/Erl/Data/Binary/Type.erl
@@ -1,4 +1,13 @@
 -module(erl_data_binary_type@foreign).
--export([eq_/2]).
+-export([eq_/2
+       , mempty_/0
+       , append_/2
+      ]).
 
 eq_(X,Y) -> X =:= Y.
+
+mempty_() -> <<>>.
+
+append_(<<>>, Bin2) -> Bin2;
+append_(Bin1, <<>>) -> Bin1;
+append_(Bin1, Bin2) -> << Bin1/binary, Bin2/binary >>.
