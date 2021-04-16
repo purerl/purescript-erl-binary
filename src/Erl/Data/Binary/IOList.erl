63eb857862c0301197dd64648ad41dc225b531f7
robashton <robashton@codeofrob.com>
2021-04-16 18:18:42 +0100

    iolist_size

--- src/Erl/Data/Binary/IOList.erl
+++ src/Erl/Data/Binary/IOList.erl
@@ -1,5 +1,5 @@
 -module(erl_data_binary_iOList@foreign).
--export([mempty_/0, append_/2, concat/1, toBinary/1, fromBinary/1]).
+-export([mempty_/0, append_/2, concat/1, toBinary/1, fromBinary/1, byteSize/1]).
 
 mempty_() -> [].
 append_(X,Y) -> [X,Y].
@@ -7,3 +7,5 @@ concat(Xs) -> Xs.
 
 fromBinary(X) -> [X].
 toBinary(X) -> erlang:iolist_to_binary(X).
+
+byteSize(X) -> iolist_size(X).
