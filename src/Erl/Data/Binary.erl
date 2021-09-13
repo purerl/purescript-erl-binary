1aa852f7f9f64956c4e06ff43150c024881e9154
srstrong <steve@srstrong.com>
2021-09-13 10:21:37 +0100

    Added part

--- src/Erl/Data/Binary.erl
+++ src/Erl/Data/Binary.erl
@@ -1,4 +1,12 @@
 -module(erl_data_binary@foreign).
--export([byteSize/1]).
+-export([byteSize/1, part_/5]).
 
 byteSize(X) -> erlang:byte_size(X).
+
+part_(Left, Right, Start, Len, Binary) ->
+    try
+        Right(binary:part(Binary, Start, Len))
+    catch
+        _:_ ->
+            Left
+    end.
--- src/Erl/Data/Binary.purs
+++ src/Erl/Data/Binary.purs
@@ -1,7 +1,13 @@
-module Erl.Data.Binary (module T, byteSize) where
+module Erl.Data.Binary (module T, byteSize, part) where
 
-import Erl.Data.Binary.Type (Binary) as T
+import Data.Maybe (Maybe(..))
 import Erl.Data.Binary.Type (Binary)
+import Erl.Data.Binary.Type (Binary) as T
 
 -- | Size in bytes of a binary
-foreign import byteSize :: Binary-> Int
+foreign import byteSize :: Binary -> Int
+
+part :: Int -> Int -> Binary -> Maybe Binary
+part = part_ Nothing Just
+
+foreign import part_ :: Maybe Binary -> (Binary -> Maybe Binary) -> Int -> Int -> Binary -> Maybe Binary
