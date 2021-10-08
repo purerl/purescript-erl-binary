e3a5da78a9264a800eb7bad918a58de5ac57ba4c
adrianroe <roe.adrian@gmail.com>
2021-10-08 19:41:20 +0100

    Added show

--- src/Erl/Data/Binary/Type.erl
+++ src/Erl/Data/Binary/Type.erl
@@ -2,6 +2,7 @@
 -export([eq_/2
        , mempty_/0
        , append_/2
+       , show_/1
       ]).
 
 eq_(X,Y) -> X =:= Y.
@@ -11,3 +12,36 @@ mempty_() -> <<>>.
 append_(<<>>, Bin2) -> Bin2;
 append_(Bin1, <<>>) -> Bin1;
 append_(Bin1, Bin2) -> << Bin1/binary, Bin2/binary >>.
+
+
+
+show_(<<>>) -> <<"<<>>">>;
+show_(Full) ->
+    case os:getenv("BINARY_SHOW_STYLE") of
+        "ERLANG" -> showErlang(Full);
+        _ ->
+            showDefault(Full)
+    end.
+
+showDefault(Full) ->
+    Size = size(Full),
+    Limit = case os:getenv("BINARY_SHOW_MAX_SIZE") of
+                false ->
+                    128;
+                Val -> try
+                           list_to_integer(Val)
+                       catch _:_ -> 128
+                       end
+            end,
+    if Size > Limit ->
+            <<Bin:(Limit-1)/binary, Last:8, _/binary>> = Full,
+            list_to_binary(io_lib:format("<<~s~s...(~p)>>", [[io_lib:format("~2.16.0B ",[X]) || <<X:8>> <= Bin ], io_lib:format("~2.16.0B",[Last]), Size]));
+       true ->
+            <<Bin:(Size-1)/binary, Last:8>> = Full,
+            list_to_binary(io_lib:format("<<~s~s>>", [[io_lib:format("~2.16.0B ",[X]) || <<X:8>> <= Bin ], io_lib:format("~2.16.0B",[Last])]))
+    end.
+
+showErlang(Full) ->
+    Size = size(Full),
+    <<Bin:(Size-1)/binary, Last:8>> = Full,
+    list_to_binary(io_lib:format("<<~s~s>>", [[io_lib:format("16#~2.16.0B,",[X]) || <<X:8>> <= Bin ], io_lib:format("16#~2.16.0B",[Last])])).
--- src/Erl/Data/Binary/Type.purs
+++ src/Erl/Data/Binary/Type.purs
@@ -7,6 +7,10 @@ foreign import data Binary :: Type
 foreign import eq_ :: Binary -> Binary -> Boolean
 foreign import mempty_ :: Binary 
 foreign import append_ :: Binary -> Binary -> Binary
+foreign import show_ :: Binary -> String
+
+instance Show Binary where
+  show = show_
 
 instance binaryEq :: Eq Binary where
   eq = eq_
